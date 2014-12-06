623c4694d9ce1279d83b431611a63d0a8dcba54a
Michael Basil <michael@intrications.com>
2014-12-06 14:52:39 +0000

    Update name to Giraff

--- app/src/androidTest/java/assembly/giraff/ApplicationTest.java
+++ app/src/androidTest/java/assembly/giraff/ApplicationTest.java
@@ -1,4 +1,4 @@
-package assembly.extintor;
+package assembly.giraff;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
--- app/src/main/java/assembly/giraff/MainActivity.java
+++ app/src/main/java/assembly/giraff/MainActivity.java
@@ -1,4 +1,4 @@
-package assembly.extintor;
+package assembly.giraff;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
